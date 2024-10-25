66e54ee3bee0cd3a2f0b4164559a92ba0ce22187
Roberto Garcia <pablo.garcia@cua.uam.mx>
2024-10-24 21:25:42 -0600

    cambio de numeros primos p y q rsa_phi

--- rsa_phi.py
+++ rsa_phi.py
@@ -3,15 +3,15 @@ from sympy import mod_inverse # biblioteca para MCD
 # funcion para MCD (maximo comun divisor con algoritmo de euclides)
 def mcd(a,b):
     while b!=0:
-        residuo= a%b # residuo de la division de a/b (17%11)
-        a= b # actualizar el valor de a, en vez de 17 va a ser 11
-        b= residuo #actualizar el valor de b, en vez de 11 va a ser el residuo de 17%11= 6
+        residuo= a%b # residuo de la division de a/b (37%29)
+        a= b # actualizar el valor de a, en vez de 37 va a ser 29
+        b= residuo #actualizar el valor de b, en vez de 29 va a ser el residuo de 37%29= 8
     return a # para este caso el MCD va a ser 1
 
 
 # paso 1: seleccionar dos numeros primos
-p= 17
-q= 11
+p= 37
+q= 29
 print(f"Los 2 numeros son: p={p} y q={q}\n")
 
 
@@ -21,7 +21,7 @@ print(f"El valor de n={p}*{q} es: {n}\n")
 
 
 # paso 3: calcular la funcion de euler phi(n)
-phi_n= (p-1)*(q-1) # 160 coprimos para n=17
+phi_n= (p-1)*(q-1) # 1008 coprimos para n=1073
 print(f"El valor de phi(n)=({p}-1)*({q}-1) es: {phi_n}\n")
 
 
@@ -31,11 +31,13 @@ for e in range(2, phi_n):
     if (mcd(e, phi_n)) == 1:
         posibles_e.append(e)
 print(f"Los {phi_n} valores posibles que se pueden tomar para e son:", posibles_e)
-e=7 # tomando 7 para e
+# e = posibles_e[0] # toma el primer valor de la lista
+e=19 # tomando 19 para e
 
 
 # paso 5: calcular d (inverso modular) 
 d=mod_inverse(e,phi_n)
+print("\nEl valor de d es: ", d)
 
 if d:
     clave_publica=(e,n)
@@ -49,7 +51,7 @@ else:
 
 
 # cifrar y descifrar mensajes
-mensaje_claro= 88
+mensaje_claro= 155
 print("\n\nEl mensaje claro es: ", mensaje_claro)
 # cifrado con clave publica {e,n}
 mensaje_cifrado= pow(mensaje_claro, e, n)  # C= (M^e) mod n
