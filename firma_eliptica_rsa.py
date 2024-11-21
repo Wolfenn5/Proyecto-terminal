73b8d89107d07a6ec54c35cecad5034ee62d6fca
Roberto Garcia <pablo.garcia@cua.uam.mx>
2024-11-20 18:30:26 -0600

    se cambio el enfoque rsa, ahora rsa cifra y descifra la firma ecdsa

--- firma_eliptica_rsa.py
+++ firma_eliptica_rsa.py
@@ -7,7 +7,7 @@ from ecdsa import SigningKey, VerifyingKey, NIST384p  # biblioteca ecdsa
 
 
 
-# generar claves rsa y ecdsa
+# generar claves rsa
 clave_privada_rsa= rsa.generate_private_key(
     public_exponent=65537, # constante de biblioteca estandarizado por el NIST: (p−1)(q−1) para exponente de cifrado y descifrado
     key_size=2048,
@@ -15,17 +15,47 @@ clave_privada_rsa= rsa.generate_private_key(
 )
 clave_publica_rsa= clave_privada_rsa.public_key()
 
-# ecdsa
+
+# cifrar firma ecdsa con RSA
+def cifrar_rsa(clave_publica_rsa, firma_ecdsa):
+    firma_ecdsa_cifrada= clave_publica_rsa.encrypt(
+        firma_ecdsa,
+        padding.OAEP( # OAEP (Optimal Asymetric Encryptation Padding)  -->  padding.PSS (probabilistic signature schema)
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    # print (cifrado) # imprime la firma cifrada pero en hexadecimal
+    return firma_ecdsa_cifrada
+
+
+# descifrar firma ecdsa con RSA
+def descifrar_rsa(clave_privada_rsa, firma_cifrada):
+    firma_descifrada= clave_privada_rsa.decrypt(
+        firma_cifrada,
+        padding.OAEP( # OAEP (Optimal Asymetric Encryptation Padding)  -->  padding.PSS (probabilistic signature schema)
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return firma_descifrada
+
+
+
+
+
+
+# generar claves ecdsa
 clave_privada_ecdsa= SigningKey.generate(curve=NIST384p)  # tipo de curva NIST384p
 clave_publica_ecdsa= clave_privada_ecdsa.get_verifying_key() # al generar la clave privada la clave publica se deriva multiplicando la clave privada por un punto generador en una curva elíptica. (P = d*G)
 
 
-
-
 # firmar con clave privada ECDSA
 def firmar_ecdsa(clave_privada_ecdsa, documento):
-    firma= clave_privada_ecdsa.sign(documento)
-    return firma # return para meter en tabla hash
+    firma_ecdsa= clave_privada_ecdsa.sign(documento)
+    return firma_ecdsa # return para meter en tabla hash
 
 
 
@@ -40,32 +70,6 @@ def verificar_ecdsa(clave_publica_ecdsa, documento, firma):
 
 
 
-# cifrar con RSA
-def cifrar_rsa(clave_publica_rsa, documento):
-    cifrado= clave_publica_rsa.encrypt(
-        documento,
-        padding.OAEP( # OAEP (Optimal Asymetric Encryptation Padding)  -->  padding.PSS (probabilistic signature schema)
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
-    )
-    return cifrado
-
-
-
-
-# descifrar con RSA
-def descifrar_rsa(clave_privada_rsa, cifrado):
-    descifrado= clave_privada_rsa.decrypt(
-        cifrado,
-        padding.OAEP( # OAEP (Optimal Asymetric Encryptation Padding)  -->  padding.PSS (probabilistic signature schema)
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
-    )
-    return descifrado
 
 
 
@@ -73,35 +77,32 @@ def descifrar_rsa(clave_privada_rsa, cifrado):
 def firma_verificacion_ecdsa(ruta_archivo):
     with open(ruta_archivo, "rb") as archivo:
         datos_archivo= archivo.read()
-
-
     # firmar archivo con ECDSA
-    firma = firmar_ecdsa(clave_privada_ecdsa, datos_archivo)
-    print(f"Firma ECDSA generada: {firma.hex()}")
+    firma_ecdsa = firmar_ecdsa(clave_privada_ecdsa, datos_archivo)
+    print(f"Firma ECDSA generada: {firma_ecdsa.hex()}")
+
+
+
+
+    # Cifrar la firma ECDSA con RSA
+    firma_ecdsa_cifrada = cifrar_rsa(clave_publica_rsa, firma_ecdsa)
+    print(f"Firma ECDSA cifrada con RSA: {firma_ecdsa_cifrada.hex()}")
+    # Descifrar la firma ECDSA con RSA
+    firma_ecdsa_descifrada = descifrar_rsa(clave_privada_rsa, firma_ecdsa_cifrada)
+    print(f"Firma ECDSA descifrada con RSA: {firma_ecdsa_descifrada.hex()}")
+
+
+
+
+
+
     # verificar firma con ECDSA
     print("\nVerificacion de firma original:")
-    verificar_ecdsa(clave_publica_ecdsa, datos_archivo, firma)
+    verificar_ecdsa(clave_publica_ecdsa, datos_archivo, firma_ecdsa)
     # modificacion en el archivo
     datos_archivo_modificados = datos_archivo + b"modificacion"
     print("\nSimulando alteracion en el archivo:")
-    verificar_ecdsa(clave_publica_ecdsa, datos_archivo_modificados, firma)
-
-
-
-
-
-# cifrado y descifrado con RSA
-def cifrado_descifrado_rsa(ruta_archivo):
-    with open(ruta_archivo, "rb") as archivo:
-        datos_archivo= archivo.read()
-
-
-    # cifrar archivo
-    cifrado= cifrar_rsa(clave_publica_rsa, datos_archivo)
-    print(f"Archivo cifrado: {cifrado.hex()}")
-    # descifrar archivo
-    descifrado= descifrar_rsa(clave_privada_rsa, cifrado)
-    print(f"Archivo descifrado: {descifrado.decode('utf-8', errors='replace')}")
+    verificar_ecdsa(clave_publica_ecdsa, datos_archivo_modificados, firma_ecdsa)
 
 
 
@@ -111,17 +112,14 @@ def cifrado_descifrado_rsa(ruta_archivo):
 
 # imagen
 def firma_verificacion_imagen(ruta_imagen):
-    print("Firma y verificacion de imagen:")
     firma_verificacion_ecdsa(ruta_imagen)
 
 # pdf
 def firma_verificacion_documento(ruta_doc):
-    print("Firma y verificacion de documento:")
     firma_verificacion_ecdsa(ruta_doc)
 
 # exe
 def firma_verificacion_exe(ruta_exe):
-    print("Firma y verificacion de exe:")
     firma_verificacion_ecdsa(ruta_exe)
 
 
@@ -133,20 +131,19 @@ def main():
     ruta_doc= "D:\\Documentos\\UAM\\PT\\Python\\PT_Planeacion.pdf"  
     ruta_exe= "D:\\Documentos\\UAM\\PT\\Python\\binario.exe"  
 
+
+
     # imagen
-    print("Firma, verificacion y simulacion de alteracion de imagen:")
+    print("Imagen jpg:")
     firma_verificacion_imagen(ruta_imagen)
-    # cifrado_descifrado_rsa(ruta_imagen)
 
     # pdf
-    print("\nFirma, verificacion y simulacion de alteracion de documento PDF:")
+    print("\nDocumento PDF:")
     firma_verificacion_documento(ruta_doc)
-    # cifrado_descifrado_rsa(ruta_doc)
 
     # exe
-    print("\nFirma, verificacion y simulacion de alteracion de exe:")
+    print("\nArchivo exe:")
     firma_verificacion_exe(ruta_exe)
-    # cifrado_descifrado_rsa(ruta_exe)
 
 
 if __name__ == "__main__":
