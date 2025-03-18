381c2e4b85ab7ad58cac349aa492c07ef9104c8f
Roberto Garcia <pablo.garcia@cua.uam.mx>
2025-03-18 00:07:14 -0600

    Se modifico el nombre de test.py a hash hex int para mejor entendimiento y se borro la parte de tabla hash como prueba temprana

--- hash_hex_int.py
+++ hash_hex_int.py
@@ -9,7 +9,3 @@ print(f"Hash SHA-256 (en hexadecimal): {hash_sha256.hex()}")
 hash_entero = int.from_bytes(hash_sha256, 'big')
 print(f"Hash como número entero: {hash_entero}")
 
-# Paso 3: Usar el valor entero para obtener un índice en la tabla hash
-tamanio_tabla_hash = 100
-indice_tabla_hash = hash_entero % tamanio_tabla_hash
-print(f"Índice en la tabla hash (con módulo {tamanio_tabla_hash}): {indice_tabla_hash}")
