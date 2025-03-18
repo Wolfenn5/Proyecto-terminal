2eb9cc6ee99af20e00ef8408bdedb321f3b8a017
Roberto Garcia <pablo.garcia@cua.uam.mx>
2025-03-18 16:44:43 -0600

    cambio de linea del mensaje cifrado en claves_rsa

--- claves_rsa.py
+++ claves_rsa.py
@@ -45,10 +45,11 @@ mensaje_descifrado_bytes= clave_privada.decrypt( # se descifra con la clave priv
     )
 )
 
-mensaje_descifrado= mensaje_descifrado_bytes.decode('utf-8')  # Convertir de bytes a cadena
 
 print("El mensaje original aun sin cifrar es:", mensaje)
 print("\nMensaje cifrado en bytes:", mensaje_cifrado) # esta linea va a imprimir el mensaje pero en bytes
 mensaje_cifrado_hex = mensaje_cifrado.hex() # convierte el mensaje cifrado a hexadecimal solo para visualizacion
 print("\nMensaje cifrado en hexadecimal:", mensaje_cifrado_hex) # imprime el mensaje pero ya cifrado y en hexadecimal
+
+mensaje_descifrado= mensaje_descifrado_bytes.decode('utf-8')  # Convertir de bytes a cadena
 print("\nMensaje descifrado:", mensaje_descifrado)
