e7a3c156cd777fc46bbef616eb532839dc656f0d
Roberto Garcia <pablo.garcia@cua.uam.mx>
2025-03-25 02:26:39 -0600

    se añadieron comentarios para describir de mejor forma el script de tabla_hash_mod_cinco

--- tabla_hash_mod_cinco.py
+++ tabla_hash_mod_cinco.py
@@ -1,72 +1,73 @@
 class TablaHash:
     def __init__(self, capacidad):
-        self.capacidad= capacidad
+        self.capacidad= capacidad # tamaño de la tabla hash, se puede cambiar 
         self.tabla= [None]*capacidad # para tener la tabla vacia
 
-
+    # Funcion hash que utliza la operacion modulo (mod) de la capacidad de la tabla hash
     def funcion_hash(self, x):
-        return x % self.capacidad # clave mod 5, 5 por la capacidad de la tabla hash
+        return x % self.capacidad # clave mod 5, 5 por la capacidad de la tabla hash, se puede cambiar 
 
 
-
+    # Funcion para insertar elementos utilizando el metodo de hashing abierto
     def insertar_hash_abierto(self, clave):
-        indice= self.funcion_hash(clave)
-        if self.tabla[indice] is None: # si no hay nada en el lugar [x]
+        indice= self.funcion_hash(clave) # se obtiene el índice inicial usando la función hash
+        if self.tabla[indice] is None: # si no hay nada en el lugar [x] se inserta
             self.tabla[indice]= [clave]  # lista de desbordamiento para cuando hay colisiones
         else:
-            self.tabla[indice].append(clave)
+            self.tabla[indice].append(clave) # si ya existe un elemento significa que hay una colision y la clave se agrega a la lista en esa posicion
 
 
-
+    # Funcion para insertar elementos utilizando el metodo de prueba lineal en hashing cerrado
     def insertar_prueba_lineal(self, clave):
-        indice= self.funcion_hash(clave)
+        indice= self.funcion_hash(clave) # se obtiene el índice inicial usando la función hash
         indice_inicial= indice
-        paso= 1
-        while self.tabla[indice] is not None:
-            indice= (indice_inicial + paso) % self.capacidad
+        paso= 1 # se indica  de cuanto va a ser el paso para cuando se itere
+        while self.tabla[indice] is not None: # mientras el espacio en la tabla esté ocupado (no esta vacio), se mueve al siguiente índice (espacio)
+            indice= (indice_inicial + paso) % self.capacidad # se busca el siguiente índice (espacio) disponible 
             paso= paso+1
-        self.tabla[indice]= clave
+        self.tabla[indice]= clave # si se encuentra un lugar vacio, se inserta 
 
 
-
+    # Funcion para insertar elementos utilizando el metodo de prueba cuadratica en hashing cerrado
     def insertar_prueba_cuadratica(self, clave):
-        indice= self.funcion_hash(clave)
+        indice= self.funcion_hash(clave) # se obtiene el índice inicial usando la función hash
         indice_inicial= indice
-        paso= 1
-        while self.tabla[indice] is not None:
+        paso= 1 # se indica  de cuanto va a ser el paso para cuando se itere
+        while self.tabla[indice] is not None: # mientras el espacio en la tabla esté ocupado (no esta vacio), se mueve al siguiente índice (espacio)
             indice= (indice_inicial+ paso**2) % self.capacidad # paso**2 = paso^2
             paso= paso+1
-        self.tabla[indice]= clave
+        self.tabla[indice]= clave # si se encuentra un lugar vacio, se inserta
 
 
-
+    # Funcion hash "secundaria" cuando se utiliza el doble hashing es decir, un segundo "hasheo"
     def funcion_doble_hash(self, clave):
-        return 1 + (clave % (self.capacidad-1))
+        return 1 + (clave % (self.capacidad-1)) # ahora sera el valor de la clave modificada (debido al doble hasheo) mod capacidad
 
 
-
+    # Funcion para insertar elementos utilizando el metodo de doble "hasheo" en hashing cerrado
     def insertar_doble_hash(self, clave):
-        indice= self.funcion_hash(clave)
-        paso= 1
-        while self.tabla[indice] is not None:
-            indice= (indice + paso * self.funcion_doble_hash(clave)) % self.capacidad
+        indice= self.funcion_hash(clave) # se obtiene el índice inicial usando la función hash
+        paso= 1 # se indica  de cuanto va a ser el paso para cuando se itere
+        while self.tabla[indice] is not None: # mientras el espacio en la tabla esté ocupado (no esta vacio), se mueve al siguiente índice (espacio)
+            indice= (indice + paso * self.funcion_doble_hash(clave)) % self.capacidad # se busca el siguiente índice disponible con doble hashing es decir, se le aplica un segundo hasheo al primer hash
             paso= paso+1
-        self.tabla[indice]= clave
+        self.tabla[indice]= clave # si se encuentra un lugar vacio, se inserta
 
 
 
-
+    # Funcion para insertar elementos utilizando el metodo de rehashing
     def rehashing(self, nueva_capacidad):
-        tabla_antigua= self.tabla
-        self.capacidad= nueva_capacidad
-        self.tabla= [None]*nueva_capacidad
+        tabla_antigua= self.tabla # se guarda la tabla original antes de cambiar la capacidad
+        self.capacidad= nueva_capacidad # se indica de cuanto va a ser la capacidad nueva. Generalmente se define como el doble de la capacidad anterior
+        self.tabla= [None]*nueva_capacidad # se crea una nueva tabla hash vacia con la nueva capacidad
+        # Se vuelven a insertar todos los elementos de la tabla hash anterior en la tabla hash nueva
         for item in tabla_antigua:
             if item is not None:
-                if isinstance(item, list):  # Para listas de desbordamiento en hashing abierto
+                if isinstance(item, list):  # si hay una lista de desbordamiento (hashing abierto)
                     for clave in item:
-                        self.insertar_hash_abierto(clave)
+                        self.insertar_hash_abierto(clave) # se inserta la clave en la tabla usando el metodo de hashing abierto
                 else:
-                    self.insertar_prueba_lineal(item)  # Puedes elegir cualquier método de inserción
+                    self.insertar_prueba_lineal(item)  # si es un valor hash unico, se inserta usando el metodo de prueba lineal
 
 
 # a modo de ejemplo, ya que en tablas hash no se indica como tal que tipo de hash se quiere al insertar
